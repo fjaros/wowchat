8990e2101fc43cda5fa1869a2c78aeffaccd3ebd
Filip Jaros <fjaros91@gmail.com>
2019-01-14 20:21:30 -0800

    v1.2.9

--- src/main/scala/wowchat/WoWChat.scala
+++ src/main/scala/wowchat/WoWChat.scala
@@ -13,7 +13,7 @@ import scala.io.Source
 
 object WoWChat extends StrictLogging {
 
-  private val RELEASE = "v1.2.8"
+  private val RELEASE = "v1.2.9"
 
   def main(args: Array[String]): Unit = {
     logger.info(s"Running WoWChat - $RELEASE")
