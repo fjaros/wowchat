fd002b1eff2d02cefc69d592da3e3fe45013d90c
Zeb Walker <zebouski@gmail.com>
2022-11-15 18:00:59 -0500

    Remove extra files

--- wowchat/run.bat
+++
@@ -1,3 +0,0 @@
-chcp 65001
-java -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8 -Dlogback.configurationFile=logback.xml -jar wowchat.jar wowchat.conf
-pause
--- wowchat/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8 -Dlogback.configurationFile=logback.xml -jar wowchat.jar wowchat.conf
